90323776e5419eda69fc32db6159f4908de67bd4
olatech97 <olatech97@gmail.com>
2022-06-22 23:13:13 -0700

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,13 +7,14 @@
  */
 int factorial(int n)
 {
-	if (n <= 1)
+	
+	if (n == 0)
 	{
-		return 1;
+		return (1);
 	}
-	else 
+	else if (n < 0)
 	{
-		return (n * factorial(n - 1));
+		return (-1);
 	}
-	return 0;
+	return (n * factorial(n - 1));
 }
