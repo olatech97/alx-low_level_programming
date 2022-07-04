4be0e2734e7663c8343a79e47485f62667f0dc0e
olatech97 <olatech97@gmail.com>
2022-07-04 08:56:43 -0700

    main_h

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
